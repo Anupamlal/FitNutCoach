7eed843ab5c6cf226faf2705417450f01300b440
Anupam Kumar Lal <anupamkumarlal@gmail.com>
2025-08-23 23:03:52 +0530

    chore: bootstrap SwiftUI app

---
+++ FitNutCoach/FitNutCoachApp.swift
@@ -0,0 +1,20 @@
+//
+//  FitNutCoachApp.swift
+//  FitNutCoach
+//
+//  Created by Anupam Kumar Lal on 23/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FitNutCoachApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
