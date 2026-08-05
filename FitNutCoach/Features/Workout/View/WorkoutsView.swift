66a46769d592f06e2a6bebc11f31b2837ed4fa62
Anupam Kumar Lal <anupamkumarlal@gmail.com>
2026-08-05 15:51:25 +0530

    Added the workout changes

--- FitNutCoach/Features/Workout/View/WorkoutsView.swift
+++ FitNutCoach/Features/Workout/View/WorkoutsView.swift
@@ -9,7 +9,123 @@ import SwiftUI
 
 struct WorkoutsView: View {
     var body: some View {
-        Text("Workouts").padding()
+        NavigationStack {
+            ZStack {
+                GeometryReader { geometry in
+                    Image("workouts_1")
+                        .resizable()
+                        .frame(width: geometry.size.width, height: geometry.size.width*2/3)
+                        .overlay {
+                            Rectangle()
+                                .foregroundStyle(Color.black.opacity(0.7))
+                        }
+                }
+                .ignoresSafeArea()
+                
+                VStack(spacing: AppSpacing.m) {
+                    Text("Day 12")
+                        .font(.system(size: 18, weight: .semibold))
+                        .foregroundStyle(Color.white)
+                    
+                    Text("Upper Body Strength")
+                        .font(.system(size: 36, weight: .semibold))
+                        .foregroundStyle(Color.white)
+                    
+                    Spacer()
+                        .frame(height: 45)
+                    
+                    Card(borderEnable: true) {
+                        HStack(alignment: .center) {
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Text("30")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundStyle(Color.textPrimary)
+                                Text("mins")
+                                    .font(.system(size: 18, weight: .medium))
+                                    .foregroundStyle(Color.textSecondary)
+                            }
+                            .frame(maxWidth: .greatestFiniteMagnitude)
+                            
+                            Spacer()
+                            
+                            Divider()
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Text("220")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundStyle(Color.textPrimary)
+                                Text("kcal")
+                                    .font(.system(size: 18, weight: .medium))
+                                    .foregroundStyle(Color.textSecondary)
+                            }
+                            .frame(maxWidth: .greatestFiniteMagnitude)
+                            
+                            Spacer()
+                            
+                            Divider()
+                            
+                            Spacer()
+                            
+                            VStack {
+                                Text("6")
+                                    .font(.system(size: 24, weight: .semibold))
+                                    .foregroundStyle(Color.textPrimary)
+                                Text("Excercises")
+                                    .font(.system(size: 18, weight: .medium))
+                                    .foregroundStyle(Color.textSecondary)
+                            }
+                            .frame(maxWidth: .greatestFiniteMagnitude)
+                            
+                            Spacer()
+                            
+                        }
+                    }
+                    .frame(height: 100)
+                    
+                    
+                    if let equipments = equipmentCategories[.chest]?.equipments {
+                        Card(borderEnable: true) {
+                            VStack(alignment: .leading, spacing: AppSpacing.l) {
+                                Text("Equipments needed")
+                                    .font(.system(size: 18, weight: .semibold))
+                                
+                                ScrollView(.horizontal, showsIndicators: false) {
+                                    HStack {
+                                        ForEach(equipments) { equipment in
+                                            Text(equipment.name)
+                                                .padding(.horizontal, 14)
+                                                .padding(.vertical, 8)
+                                                .background(equipment.color)
+                                                .foregroundColor(.white)
+                                                .clipShape(Capsule())
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        .frame(height: 100)
+                    }
+                    
+                    FNButton(buttonTitle: "Start Workout", backgroundEnable: true) {
+                        
+                    }
+                    
+                    FNButton(buttonTitle: "Change Workout", backgroundEnable: false) {
+                        
+                    }
+                    
+                    Spacer()
+                    
+                }
+                .padding(.horizontal)
+                
+            }
+        }
     }
 }
 
